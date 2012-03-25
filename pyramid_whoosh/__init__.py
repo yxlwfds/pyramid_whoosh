1b6318d853b63ac1d59b7cc87aac50fd7d4cc34e
Rach Belaid <rachid.belaid@gmail.com>
2012-03-25 23:23:16 +0100

    setup module to check settings and attach indexer when it is included

--- pyramid_whoosh/__init__.py
+++ pyramid_whoosh/__init__.py
@@ -1,10 +1,27 @@
-from pyramid.config import Configurator
+import os
+from whoosh import index
 
-def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-    config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.scan()
-    return config.make_wsgi_app()
+_PREFIX="whoosh"
+def _base_name(string):
+        return '%s.%s' % (prefix, string)
+
+def check_whoosh_env_from_settings(settings):
+    if not _base_name('index_dir') in settings:
+        raise AttributeError("You need to provide whoosh.index_dir in your configuration")
+
+    index_dir = settings.get(_base_name('index_dir'))
+    if not os.path.exists(index_dir):
+        raise AttributeError("The directory '%s' doesn't exist, you need to create it")
+
+
+
+def includeme(config):
+    check_whoosh_env_from_settings(config.registry.settings)
+    config.add_subscriber(add_whoosh_indexer, 'pyramid.events.NewRequest')
+
+
+
+def add_whoosh_indexer(event):
+    settings = event.request.registry.settings
+    index_dir = settings.get(_base_name('index_dir'))
+    event.request.whoosh_ix =  index.open_dir(index_dir)
